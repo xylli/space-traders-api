276b2b15e906a13f75550dad220333f55351edd3
Peter Suepfle <peter.suepfle@gmail.com>
2023-05-09 13:31:19 +0200

    project init

---
+++ src/main/java/com/deatr/xylli/spacetradersapi/SpaceTradersApiApplication.java
@@ -0,0 +1,13 @@
+package com.deatr.xylli.spacetradersapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceTradersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceTradersApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deatr/xylli/spacetradersapi/SpaceTradersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deatr.xylli.spacetradersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceTradersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
