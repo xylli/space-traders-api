c6993116cd4340f72a658ce401087b37d7c2d5e0
xylli <peter.suepfle@gmail.com>
2023-10-22 23:54:18 +0200

    add kotlin gradle dependencies

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,4 +11,9 @@ dependencies {
     implementation("com.github.spotbugs.snom:spotbugs-gradle-plugin:6.0.0-beta.4")
     implementation("org.springframework.boot:spring-boot-gradle-plugin:3.1.4")
     implementation("io.spring.gradle:dependency-management-plugin:1.1.3")
+
+    //kotlin
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
+    // required for K2JVMCompiler::class - will be provided at runtime by Gradle
+    compileOnly("org.jetbrains.kotlin:kotlin-compiler-embeddable:1.9.10")
 }
