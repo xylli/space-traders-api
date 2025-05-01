49235c1f3941732c060890c4bfaf241536dbd29c
Peter Suepfle <peter.suepfle@gmail.com>
2025-05-01 17:00:01 +0200

    Upgrade gradle + add db dependencies

--- space-traders-service/build.gradle.kts
+++ space-traders-service/build.gradle.kts
@@ -12,13 +12,23 @@ version = "0.0.2-SNAPSHOT"
 
 dependencies {
 	implementation(project(":space-traders-client"))
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	runtimeOnly("com.h2database:h2")
+	implementation("org.springframework.session:spring-session-core")
+
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.session:spring-session-jdbc")
+	implementation("org.liquibase:liquibase-core")
+	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+	runtimeOnly("org.postgresql:postgresql")
+
 	testImplementation("org.springframework.security:spring-security-test")
+	testImplementation("org.springframework.boot:spring-boot-testcontainers")
+	testImplementation("org.testcontainers:postgresql")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	testRuntimeOnly("com.h2database:h2")
 }
 
 tasks.withType<Test> {
